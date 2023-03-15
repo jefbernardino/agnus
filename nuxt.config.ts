fa043eddec0ab7db3bea4599d825c151ad8e95c3
Jeferson Bernardino <jefbernardino@gmail.com>
2023-03-14 22:22:40 -0300

    Code review

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,13 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
     privateRuntimeConfig: {
         mysql: {
-            host: 'mysql.agnusplast.com.br',
-            user: 'agnus365_megalu',
-            password: 'megalu2211',
-            database: 'agnus365_novosite',
+            host: "mysql.agnusplast.com.br",
+            user: "agnus365_megalu",
+            password: "megalu2211",
+            database: "agnus365_novosite",
         },
     },
-    target: 'static',
+    target: "static",
     ssr: false,
     build: {
         extractCSS: false,
@@ -18,8 +18,8 @@ export default defineNuxtConfig({
     },
     css: ["@/assets/scss/style.scss"],
     modules: [
-      '@pinia/nuxt',
-      '@pinia-plugin-persistedstate/nuxt',
+      "@pinia/nuxt",
+      "@pinia-plugin-persistedstate/nuxt",
     ],
     vite: {
         define: {
